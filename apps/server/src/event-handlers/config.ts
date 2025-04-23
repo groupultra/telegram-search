0b64735e3b026608c4b62e6248fdb62e8d1f8cfe
hahaqwq <hahaqwq233@gmail.com>
2025-04-23 19:57:08 +0800

    feat: update deps

--- apps/server/src/event-handlers/config.ts
+++ apps/server/src/event-handlers/config.ts
@@ -1,7 +1,8 @@
-import type { ClientState } from '../app'
-import type { WsMessageToServer } from './ws-event'
+import type { Config } from '@tg-search/common'
+import type { WsMessageToServer } from '../utils/ws-event'
+import type { ClientState } from '../ws'
 
-import { sendWsError, sendWsEvent } from './ws-event'
+import { sendWsError, sendWsEvent } from '../utils/ws-event'
 
 export function registerConfigEventHandler(state: ClientState) {
   const { peer, ctx } = state
@@ -12,7 +13,7 @@ export function registerConfigEventHandler(state: ClientState) {
 
   const { emitter } = ctx
 
-  emitter.on('config:result', (data) => {
+  emitter.on('config:result', (data: { config: Config }) => {
     sendWsEvent(peer, 'config:result', data)
   })
 }
