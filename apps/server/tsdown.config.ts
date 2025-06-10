f726eb6adabf5301392d1babe910c578e7c0e511
RainbowBird <git@luoling.moe>
2025-06-11 02:16:18 +0800

    chore: fixedExtension

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -5,4 +5,5 @@ export default defineConfig({
   exports: {
     devExports: true,
   },
+  fixedExtension: true,
 })
--- packages/common/tsdown.config.ts
+++ packages/common/tsdown.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
+  entry: [
+    './src/index.ts',
+    './src/composable/index.ts',
+  ],
   sourcemap: true,
   exports: {
     devExports: true,
   },
+  fixedExtension: true,
 })
