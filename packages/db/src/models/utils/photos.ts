7ac3639abaa2ad279f70933acfdca9b816ca06a8
RainbowBird <git@luoling.moe>
2025-07-03 17:46:48 +0800

    refactor: remove photo base64

--- packages/db/src/models/utils/photos.ts
+++ packages/db/src/models/utils/photos.ts
@@ -8,19 +8,12 @@ import { Buffer } from 'node:buffer'
 export type DBInsertPhoto = typeof photosTable.$inferInsert
 export type DBSelectPhoto = typeof photosTable.$inferSelect
 
-export function bufferToBase64(buffer: Buffer<ArrayBufferLike>): string {
-  return buffer.toString('base64')
-}
-
-export function base64ToBuffer(base64: string): Buffer<ArrayBufferLike> {
-  return Buffer.from(base64, 'base64')
-}
 
 export function convertDBPhotoToCoreMessageMedia(dbPhoto: DBSelectPhoto): CoreMessageMedia {
   return {
     type: 'photo',
     messageUUID: dbPhoto.message_id as UUID,
-    base64: dbPhoto.image_bytes ? bufferToBase64(dbPhoto.image_bytes) : undefined,
+    byte: dbPhoto.image_bytes ? Buffer.from(dbPhoto.image_bytes) : undefined,
     path: dbPhoto.image_path || undefined,
   } satisfies CoreMessageMedia
 }
