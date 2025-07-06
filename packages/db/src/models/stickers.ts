05069a68820fdadd55000f61c0a44abf82e2f64a
hahaqwq <3109944373@qq.com>
2025-07-06 13:18:25 +0800

    feat: bluk insert
    Signed-off-by: hahaqwq <3109944373@qq.com>

--- packages/db/src/models/stickers.ts
+++ packages/db/src/models/stickers.ts
@@ -5,7 +5,7 @@ import type { CoreMessageMedia } from '../../../core/src'
 import { Buffer } from 'node:buffer'
 
 import { Ok } from '@tg-search/common/utils/monad'
-import { desc, eq } from 'drizzle-orm'
+import { desc, eq, sql } from 'drizzle-orm'
 
 import { withDb } from '../drizzle'
 import { recentSentStickersTable } from '../schemas/recent_sent_stickers'
@@ -62,7 +62,31 @@ export async function recordSticker(sticker: StickerMedia) {
 }
 
 export async function recordStickers(stickers: StickerMedia[]) {
-  return Promise.all(stickers.map(sticker => recordSticker(sticker)))
+  if (stickers.length === 0) {
+    return []
+  }
+
+  return withDb(async db => db
+    .insert(stickersTable)
+    .values(stickers.map(sticker => ({
+      platform: 'telegram',
+      file_id: sticker.sticker_id,
+      sticker_bytes: sticker.byte ?? Buffer.from(''),
+      sticker_path: '',
+      description: '',
+      name: '',
+      emoji: sticker.emoji ?? '',
+      label: '',
+    })))
+    .onConflictDoUpdate({
+      target: [stickersTable.file_id],
+      set: {
+        sticker_bytes: sql`EXCLUDED.sticker_bytes`,
+        sticker_path: sql`EXCLUDED.sticker_path`,
+      },
+    })
+    .returning(),
+  )
 }
 
 export async function listRecentSentStickers() {
