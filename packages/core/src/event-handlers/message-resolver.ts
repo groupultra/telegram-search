a600ecfa99cf7bd69f62817e1be266fcbb3a4039
RainbowBird <git@luoling.moe>
2025-10-17 20:19:49 +0800

    chore: catch error

--- packages/core/src/event-handlers/message-resolver.ts
+++ packages/core/src/event-handlers/message-resolver.ts
@@ -1,19 +1,28 @@
 import type { CoreContext } from '../context'
 import type { MessageResolverService } from '../services/message-resolver'
 
+import { useLogger } from '@unbird/logg'
 import pLimit from 'p-limit'
 
 import { MESSAGE_PROCESS_LIMIT } from '../constants'
 
 export function registerMessageResolverEventHandlers(ctx: CoreContext) {
   const { emitter } = ctx
+  const logger = useLogger('core:message-resolver:event')
 
   return (messageResolverService: MessageResolverService) => {
     const limit = pLimit(MESSAGE_PROCESS_LIMIT)
 
     // TODO: debounce, background tasks
     emitter.on('message:process', ({ messages }) => {
-      limit(() => messageResolverService.processMessages(messages))
+      limit(async () => {
+        try {
+          await messageResolverService.processMessages(messages)
+        }
+        catch (error) {
+          logger.withError(error).warn('Failed to process messages')
+        }
+      })
     })
   }
 }
