7f73e7e15572616136f5f4322c77226879340770
RainbowBird <git@luoling.moe>
2025-10-17 20:15:52 +0800

    feat: p-limit

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,1 +1,2 @@
 export const MESSAGE_PROCESS_BATCH_SIZE = 100
+export const MESSAGE_PROCESS_LIMIT = 10
--- packages/core/src/event-handlers/message-resolver.ts
+++ packages/core/src/event-handlers/message-resolver.ts
@@ -1,13 +1,19 @@
 import type { CoreContext } from '../context'
 import type { MessageResolverService } from '../services/message-resolver'
 
+import pLimit from 'p-limit'
+
+import { MESSAGE_PROCESS_LIMIT } from '../constants'
+
 export function registerMessageResolverEventHandlers(ctx: CoreContext) {
   const { emitter } = ctx
 
   return (messageResolverService: MessageResolverService) => {
+    const limit = pLimit(MESSAGE_PROCESS_LIMIT)
+
     // TODO: debounce, background tasks
     emitter.on('message:process', ({ messages }) => {
-      messageResolverService.processMessages(messages)
+      limit(() => messageResolverService.processMessages(messages))
     })
   }
 }
