ad865f3cce10255bdd145242c9894529732f87fb
RainbowBird <git@luoling.moe>
2025-12-03 20:42:07 +0800

    chore: watch active session id

--- packages/client/src/stores/useAccountSettings.ts
+++ packages/client/src/stores/useAccountSettings.ts
@@ -1,15 +1,24 @@
+import { useLogger } from '@guiiai/logg'
 import { generateDefaultAccountSettings } from '@tg-search/core'
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { defineStore, storeToRefs } from 'pinia'
+import { ref, watch } from 'vue'
 
 import { useBridgeStore } from '../composables/useBridge'
 
 export const useAccountStore = defineStore('account', () => {
   const accountSettings = ref(generateDefaultAccountSettings())
+  const { activeSessionId } = storeToRefs(useBridgeStore())
 
   function init() {
     useBridgeStore().sendEvent('config:fetch')
   }
+
+  watch(activeSessionId, (newId) => {
+    if (newId) {
+      useLogger('AccountStore').log('Fetching config for new session')
+      useBridgeStore().sendEvent('config:fetch')
+    }
+  })
 
   return {
     init,
